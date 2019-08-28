b0fc2e4b515eb019b4028f1791f231a90c812705
Daniil Manin <manindaniil@protonmail.com>
2019-08-28 14:26:28 +0700

    Initial Commit

---
+++ AppleCardReplicating/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleCardReplicating
+//
+//  Created by Daniil Manin on 8/28/19.
+//  Copyright © 2019 Exyte. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
